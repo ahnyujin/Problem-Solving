73264ebcf7b25f92da2f7d88d3bb0ec8a1d26765
Yujin Ahn <ujahnn@gmail.com>
2025-04-17 21:34:43 +0900

    Solved LeetCode 2176

---
+++ LeetCode/2176_CountEqualAndDivisiblePairsInAnArray/Solution.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int countPairs(vector<int>& nums, int k) {
+        int size = nums.size();
+        int ret = 0;
+        for(int i = 0; i < size; i++) {
+            for(int j = i + 1; j < size; j++) {
+                if (nums[i] == nums[j] && i * j % k == 0){
+                    ret++;
+                }
+            }
+        }
+        return ret;
+    }
+};
